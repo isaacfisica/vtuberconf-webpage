5e886d2748900e0994bfb5003a905cdc1849fa22
isaacfisica <isaac.fisica.kr@gmail.com>
2026-06-26 11:34:00 +0200

    로빈 내용 추가

--- src/lib/members/robin.ts
+++ src/lib/members/robin.ts
@@ -4,7 +4,7 @@ const robin: Member = {
   id: 'robin',
   name: '네무이 로빈',
   field: 'HCI (인간-컴퓨터 상호작용)',
-  degree: '이학석사',
+  degree: '공학석사',
   deg: 'ms',
   tags: ['HCI', '인지과학', '디자인'],
   role: '정회원',
@@ -12,6 +12,10 @@ const robin: Member = {
   bio: '사람을 연구하는 일을 업으로 삼는 HCI 전공 석사입니다.',
   bioLong:
     'HCI(인간-컴퓨터 상호작용)를 전공한 석사로, 지금도 사람을 연구하는 일을 직업으로 삼고 있습니다. 현재는 스트리밍 없이 유튜브 편집 콘텐츠를 올리고 있습니다.',
+     sns: [
+    { kind: 'X', label: '@nemui_robin', url: 'https://x.com/nemui_robin' },
+    { kind: 'YouTube', label: '@nemui_robin', url: 'https://www.youtube.com/@nemui_robin' },
+  ],
 };
 
 export default robin;
